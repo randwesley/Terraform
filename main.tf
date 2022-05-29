52cae0e52ad3ef17b66f629d57977828c931a32b
Randerson Reis <randwesley0@gmail.com>
2022-05-29 17:53:34 -0300

    Configuração do resource aws_instance

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-09d56f8956ab235b3"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    "name" = "dev${count.index}"
+  }
+  vpc_security_group_ids = [ "sg-0d883e0ca7448654d" ]
+}
